f41d9fc7e353231ab20120438649b230f54d27af
GuidoYebara <guido@DESKTOP-P8G50EP.localdomain>
2021-04-06 20:13:48 -0300

    Primer commit

---
+++ MacOwins/MacOwins.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MacOwins
+{
+    class MacOwins
+    {
+        List<Sucursal> sucursales;
+
+
+        public MacOwins()
+        {
+            sucursales = new List<Sucursal>();
+        }
+
+
+    }
+}
